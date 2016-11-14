dbb2fcfab2908cacf2a16a2e5ac4d10fd717f86d
Anna Mendoza <annamendoza.curve@gmail.com>
2016-11-14 14:21:38 -0800

    cleaning master

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-//var ReactRouter = require('react-router');
-// var Router = ReactRouter.Router;
-// var Route = ReactRouter.Route;
-// var IndexRoute = ReactRouter.IndexRoute;
-
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
