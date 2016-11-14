aa859f66fe911ef33a9c06e6041633c713f61363
Anna Mendoza <annamendoza.curve@gmail.com>
2016-11-14 01:53:39 -0800

    getting that router to work

--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var USER_DATA = {
-	name: 'PinkyBoy McGinnis',
-	username: 'tylermcginnis',
-	image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
-}
-
-/*
-	Focused
-	Independent
-	Reusable
-	Small
-	Testable
-*/
+//var routes = require('./config/routes');
+import routes from './config/routes';
 
 // var HelloWorld = React.createClass({
 // 	render: function () {
@@ -24,86 +13,71 @@ var USER_DATA = {
 // 	}
 // });
 
-var Link = React.createClass({
-	changeURL: function() {
-		window.location.replace(this.props.href)
-	},
-	render: function() {
-		return (
-			<span 
-				style={{color: 'blue', cursor: 'pointer'}}
-				onClick={this.changeURL}>
-				{this.props.children}
-			</span>
-		)
-	}
-});
+// ****************************************************
+
+// var USER_DATA = {
+// 	name: 'PinkyBoy McGinnis',
+// 	username: 'tylermcginnis',
+// 	image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+// }
+
+// var Link = React.createClass({
+// 	changeURL: function() {
+// 		window.location.replace(this.props.href)
+// 	},
+// 	render: function() {
+// 		return (
+// 			<span 
+// 				style={{color: 'blue', cursor: 'pointer'}}
+// 				onClick={this.changeURL}>
+// 				{this.props.children}
+// 			</span>
+// 		)
+// 	}
+// });
 
 
-var ProfilePic = React.createClass({
-	render: function () {
-		return <img src={this.props.imageUrl} style={{height: 100, width:100}} />
-	}
-});
+// var ProfilePic = React.createClass({
+// 	render: function () {
+// 		return <img src={this.props.imageUrl} style={{height: 100, width:100}} />
+// 	}
+// });
 
-var ProfileLink = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<Link href={'https://www.github.com/' + this.props.username}>
-				  {this.props.username}
-				</Link>  
-			</div>
-		)
-	}
-});
+// var ProfileLink = React.createClass({
+// 	render: function () {
+// 		return (
+// 			<div>
+// 				<Link href={'https://www.github.com/' + this.props.username}>
+// 				  {this.props.username}
+// 				</Link>  
+// 			</div>
+// 		)
+// 	}
+// });
 
-var ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>{this.props.name}</div>
-		)
-	}
-});
+// var ProfileName = React.createClass({
+// 	render: function () {
+// 		return (
+// 			<div>{this.props.name}</div>
+// 		)
+// 	}
+// });
 
-var Avatar = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileName name={this.props.user.name} />
-				<ProfileLink username={this.props.user.username} />
-			</div>	
-		)
-	}
-});
+// var Avatar = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div>
+// 				<ProfilePic imageUrl={this.props.user.image} />
+// 				<ProfileName name={this.props.user.name} />
+// 				<ProfileLink username={this.props.user.username} />
+// 			</div>	
+// 		)
+// 	}
+// }); ****************************************************
 
 ReactDOM.render(
 	//<HelloWorld name="Tyler" />,  // works with HelloWorld
-	< Avatar user={USER_DATA} />,
+	// < Avatar user={USER_DATA} />, // works with Avatar, and everything between the stars
+	routes,
 	document.getElementById('root')
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+);
