3785387f5307885d976334de77566532120825a4
Anna Mendoza <annamendoza.curve@gmail.com>
2016-11-14 14:14:04 -0800

    updated the index.js

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ import routes from './config/routes';
 // }); ****************************************************
 
 ReactDOM.render(
-	//<HelloWorld name="Tyler" />,  // works with HelloWorld
+	// <HelloWorld name="Tyler" />,  // works with HelloWorld
 	// < Avatar user={USER_DATA} />, // works with Avatar, and everything between the stars
 	routes,
 	document.getElementById('root')
