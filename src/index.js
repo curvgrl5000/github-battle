9f80d8c3b31ba1ac6ca464cfb0b3dd4be5b02b48
Anna Mendoza <annamendoza.curve@gmail.com>
2016-11-13 23:13:47 -0800

    added the custom Link and updated the function with a changeURL function that replaces props.href

--- src/index.js
+++ src/index.js
@@ -24,6 +24,21 @@ var USER_DATA = {
 // 	}
 // });
 
+var Link = React.createClass({
+	changeURL: function() {
+		window.location.replace(this.props.href)
+	},
+	render: function() {
+		return (
+			<span 
+				style={{color: 'blue', cursor: 'pointer'}}
+				onClick={this.changeURL}>
+				{this.props.children}
+			</span>
+		)
+	}
+});
+
 
 var ProfilePic = React.createClass({
 	render: function () {
@@ -35,9 +50,9 @@ var ProfileLink = React.createClass({
 	render: function () {
 		return (
 			<div>
-				<a href={'https://www.github.com/' + this.props.username}>
+				<Link href={'https://www.github.com/' + this.props.username}>
 				  {this.props.username}
-				</a>  
+				</Link>  
 			</div>
 		)
 	}
